8f0612cfe80fe1a39da59e2f1919684f31204251
Behrooz Nobahar <109895703+behrooznobahar@users.noreply.github.com>
2022-08-14 00:25:58 +0430

    Update Bot_Login_form.py

--- Bot_Login_form.py
+++ Bot_Login_form.py
@@ -4,18 +4,22 @@ from tkinter import *
 import tkinter.messagebox as mesagebox
 import os
 from PIL import ImageTk,Image
+
 root = Tk()
 root.geometry('600x400')
 root.title('Botich')
-wlcomel = Label(root, text='Welcom \n I am Botich, Congratulations on your choice',bg='#AA1923', width=len('I am Botich, Congratulations on your choice')+10,pady=10)
+root.config(bg='green')
+
+img = PhotoImage(file="botich.jpg")
+label = Label(
+    root,
+    image=img
+)
+label.place(x=-1, y=-1 )
+wlcomel = Label(root, text='Welcom \n I am Botich, Congratulations on your choice', width=len('I am Botich, Congratulations on your choice')+10,pady=10)
 wlcomel.place(relx=0.5,rely=0.1,anchor=CENTER)
 btn_register=Button(text='Register',command='run',padx=10,bg='#AA1923',width=10)
 btn_register.place(relx=0.6,rely=0.3,anchor=CENTER)
 btn_log_in=Button(text='Login',command='run',padx=10,bg='#AA1923',width=10)
 btn_log_in.place(relx=0.4,rely=0.3,anchor=CENTER)
-image=Image.open('botich.jpg')
-image.resize((20,20),Image.ANTIALIAS)
-userimg=ImageTk.PhotoImage(image)
-userimg=Label(root,image=userimg,width=100)
-userimg.place(relx=0.5,rely=0.8,anchor=CENTER)
 root.mainloop()
